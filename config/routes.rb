a9df2cc1819b7249de9513052cac5ad5d7d680d9
somedayrainbows <ebassity@gmail.com>
2017-05-03 17:12:58 -0600

    Add items relationships endpoints
    * Namespaced invoice_items and merchant under items
    * Updated items request spec to include endpoint tests
    * Added invoice_items controller under items directory
    * Added merchants controller under items directory

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,17 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
       namespace :merchants, only: [:index, :show] do
-        get '/:id/items', to: 'items#index', as: 'api_v1_merchant_items'
-        get '/:id/invoices', to: 'invoices#index', as: 'api_v1_merchant_invoices'
+        get '/:id/items', to: 'items#index', as: 'merchant_items'
+        get '/:id/invoices', to: 'invoices#index', as: 'merchant_invoices'
       end
       resources :invoices, only: [:index, :show]
       resources :transactions, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :items, only: [:index, :show]
+      namespace :items do
+        get '/:id/invoice_items', to: 'invoice_items#index', as: 'item_invoice_items'
+        get '/:id/merchant', to: 'merchants#show', as: 'item_merchant'
+      end
       resources :invoice_items, only: [:index, :show]
     end
   end
