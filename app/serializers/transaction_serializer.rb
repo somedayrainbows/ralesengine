ab88768199a9f7905e8897056ff262103bbee268
somedayrainbows <ebassity@gmail.com>
2017-05-03 10:14:27 -0600

    Updates serializer to remove cc exp date
    * Remove cc exp date from serializer because all fields in column are empty in CSV

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,3 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result
+  attributes :id, :invoice_id, :credit_card_number, :result
 end
