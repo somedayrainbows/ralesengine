6e36c5d919d7d28b0f9570608b44e4b91153a63c
dannyradden <dradden@gmail.com>
2017-05-03 15:00:38 -0600

    Add logic for Invoice relationship JSON outputs * Can get invoice transactions * Can get invoice invoice_items * Can get invoice items * Can get invoice customer * Can get invoice merchant
    Add logic for invoice_items relationship JSON outputs
    * Can get invoice_items invoice
    * Can get invoice_items item

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,26 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
-      namespace :merchants, only: [:index, :show] do
-        get '/:id/items', to: 'items#index', as: 'api_v1_merchant_items'
-        get '/:id/invoices', to: 'invoices#index', as: 'api_v1_merchant_invoices'
+      namespace :merchants do
+        get '/:id/items', to: 'items#index', as: 'items'
+        get '/:id/invoices', to: 'invoices#index', as: 'invoices'
       end
       resources :invoices, only: [:index, :show]
+      namespace :invoices do
+        get '/:id/transactions', to: 'transactions#index', as: 'transactions'
+        get '/:id/invoice_items', to: 'invoice_items#index', as: 'invoice_items'
+        get '/:id/items', to: 'items#index', as: 'items'
+        get '/:id/customer', to: 'customers#show', as: 'customer'
+        get '/:id/merchant', to: 'merchants#show', as: 'merchant'
+      end
       resources :transactions, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
+      namespace :invoice_items do
+        get '/:id/invoice', to: 'invoices#show', as: 'invoice'
+        get '/:id/item', to: 'items#show', as: 'item'
+      end
     end
   end
 end
