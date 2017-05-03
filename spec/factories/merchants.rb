11f89081f4866c4acdc46e2399f7e1255f827d4e
somedayrainbows <ebassity@gmail.com>
2017-05-02 19:35:38 -0600

    Add relationship among merchant, invoices, items
    * Set up should-matchers
    * Set up item belongs_to merchant
    * Set up invoice belongs_to merchant
    * Set up merchant has_many items, invoices
    * Ran migrations to add merchant_id to items, invoices
    *

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :merchant do
     sequence :name do |n|
       "Merchant #{n}"
+      invoices { [association(:invoice)] }
+      items { [association(:invoice)] }
     end
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe "Items API" do
   it "sends a list of items" do
-    create_list(:item, 3)
+    merchant = Merchant.create(name: 'Lady Jane')
+    items = create_list(:item, 3, merchant: merchant)
 
     get '/api/v1/items'
 
@@ -14,13 +15,15 @@ describe "Items API" do
   end
 
   it "can get one item by its id" do
-    id = create(:item).id
+    merchant = Merchant.create(name: 'Lady Jane')
+    item = create_list(:item, 3, merchant: merchant).first
+    id = item.id
 
     get "/api/v1/items/#{id}"
 
     item = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["id"]).to eq(id)  
+    expect(item["id"]).to eq(id)
   end
 end
