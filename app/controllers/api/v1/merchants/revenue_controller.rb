d47355279690fd412cb724bdf6afbc6b0be409f3
dannyradden <dradden@gmail.com>
2017-05-04 14:02:18 -0600

    Add Merchant Revenue By Date * Add .where(hash) to revenue methods in merchant model where hash contains a date if the param exists * Date passed in through revenue controller with date_finder method. * Add merchant revenue by datespec

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-    render json: Merchant.find(params[:id]), serializer: RevenueSerializer
+
+    render json: Merchant.find(params[:id]).revenue(date_finder), serializer: RevenueSerializer
   end
 
+  private
+
+  def date_finder
+    {created_at: params['date']} if params.key?('date')
+  end
 end
