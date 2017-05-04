4bf7b28c85f4e9ff86b3b8b3193e4b525c350875
somedayrainbows <ebassity@gmail.com>
2017-05-03 18:28:07 -0600

    Finishes transaction and customer relationship endpoints
    * Added has_many transactions to customer model
    * Added routes namespacing for invoice under transactions
    * Added routes namespacing for invoices and transactions under customers
    * Updated customer request spec with tests
    * Updated transactions request spec with tests
    * Added customer directory and transactions & invoices controllers
    * Added transactions directory and invoices controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,14 @@ Rails.application.routes.draw do
       end
       resources :invoices, only: [:index, :show]
       resources :transactions, only: [:index, :show]
+      namespace :transactions do
+        get '/:id/invoice', to: 'invoices#show', as: 'transaction_invoice'
+      end
       resources :customers, only: [:index, :show]
+      namespace :customers do
+        get '/:id/invoices', to: 'invoices#index', as: 'customer_invoices'
+        get '/:id/transactions', to: 'transactions#index', as: 'customer_transactions'
+      end
       resources :items, only: [:index, :show]
       namespace :items do
         get '/:id/invoice_items', to: 'invoice_items#index', as: 'item_invoice_items'
