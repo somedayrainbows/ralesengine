db5449655ca1393229c1bbb98061ce9a8ff2d6a8
dannyradden <dradden@gmail.com>
2017-05-03 21:22:12 -0600

    Add find and random logic for invoices  * Add result and credit card number to param finder in application controller  * Add sequence dates to invoices factory  * Add spec for find, find_all, and random  * Add routes and controllers for find and random in invoices namespace

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
       resources :invoices, only: [:index, :show]
       namespace :transactions do
         get '/:id/invoice', to: 'invoices#show', as: 'transaction_invoice'
+        get 'find', to: 'finder#show'
+        get 'find_all', to: 'finder#index'
+        get 'random', to: 'random#show'
       end
       resources :transactions, only: [:index, :show]
       namespace :customers do
