db0756b06fe6af142f4095981e76711ea4688ca2
somedayrainbows <ebassity@gmail.com>
2017-05-04 22:35:02 -0600

    Adds total revenue by date all merchants
    * Added self.revenue method to merchant model
    * Updated routes for revenue#index
    * Added test to merchant request spec

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
         get '/most_items', to: 'most_items#index'
         get '/:id/customers_with_pending_invoices', to: 'customers#index'
         get '/most_revenue', to: 'most_revenue#index'
+        get '/revenue', to: 'revenue#index'
       end
       resources :merchants, only: [:index, :show]
       namespace :invoices do
